4ae28071a50977a5ad41bbf679c782b1c1b9404e
christofferPerch <cph-cn332@cphbusiness.dk>
2024-09-06 09:27:43 +0200

    Add project files.

---
+++ Sem2_Systems-Integration/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sem2_Systems_Integration.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
